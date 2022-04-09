62aee7a0a79415811a0dd624131936e5e04aacb0
Jeff Lau <jeff-lau@live.com>
2022-04-09 12:29:32 +0800

    Create components page

---
+++ frontend/components/Base.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    background: black;
+    color: white;
+    height: 100%:
+    padding: 20px;
+`
+
+export const Stepper = styled.div`
+    background: #FCE9A6;
+    color: black;
+    padding: 5px;
+    display: flex;
+    
+    &:before {
+        border: 1px solid #FCE9A6;
+    }
+`
---
+++ frontend/pages/components/index.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import { Wrapper, Stepper } from '../../components/Base'
+
+const Components = () => {
+ 
+  return <Wrapper>
+      Some stuff
+      <Stepper>Step1 of 2</Stepper>
+  </Wrapper>
+}
+
+export default Components
