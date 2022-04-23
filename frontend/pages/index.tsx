8527221eb8b2f26bd421420f0395f701e5929d3e
Uma <uma.roy.us@gmail.com>
2022-04-23 04:27:43 +0200

    FInal touches on homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -39,21 +39,21 @@ const Homepage = () => {
       </div>
       <div className="-mt-24 flex h-full items-center justify-center bg-black text-white">
         <div className="max-w-3xl">
-          <SimpleRow title="What is cabal?">
-            Cabal is a discord bot you can add to your server to create{' '}
-            <i>credibly pseudonymous</i> channels based on their Ethereum
+          <SimpleRow title="What is Cabal?">
+            Cabal is a Discord bot you can add to your server to create{' '}
+            <i>credibly pseudonymous</i> channels based on members' Ethereum
             activity.
             <div>
               <span className="text-terminal-green">{'>>>'} </span>
               <span>
-                Users can join these channels by providing a zero-knowledge
-                proof showing that they carried out some action on Ethereum
-                without revealing their address.
+                Members can join these channels by providing a zero-knowledge
+                proof showing that they satisify some property (or carried out
+                some action) on Ethereum without revealing their address.
               </span>
             </div>
           </SimpleRow>
 
-          <SimpleRow title="How do I use it to manage my gated-discord roles (for admins)?">
+          <SimpleRow title="(For admins) How do I use Cabal to manage my gated Discord roles?">
             <a
               href="https://www.notion.so/0xparc/Configuring-Cabal-2554b43826e9431e934062452f07be65"
               className="text-gray-200 hover:text-gray-400"
@@ -62,7 +62,7 @@ const Homepage = () => {
               Guide: Configuring Cabal
             </a>
           </SimpleRow>
-          <SimpleRow title="How do I use it to authenticate a discord server (for users)?">
+          <SimpleRow title="(For users) How do I use Cabal to get verified in a Discord server?">
             <a
               href="https://0xparc.notion.site/Getting-verified-with-Cabal-64496bf7d95b4481bfc3c3a6cb3eae51"
               className="hover:text-gray-400"
@@ -74,16 +74,30 @@ const Homepage = () => {
           <SimpleRow title="Blog Post">
             Coming soon! Follow{' '}
             <a
+              href="https://twitter.com/cabalxyz"
+              target="_blank"
+              className="hover:text-gray-400"
+            >
+              @cabalxyz
+            </a>{' '}
+            and{' '}
+            <a
               href="https://twitter.com/0xPARC"
               target="_blank"
               className="hover:text-gray-400"
             >
               @0xPARC
             </a>{' '}
-            for more
+            for more.
           </SimpleRow>
           <SimpleRow title="Open Source Resources">
-            https://github.com/0xparc/cabal
+            <a
+              href="https://github.com/0xparc/cabal"
+              target="_blank"
+              className="hover:text-gray-400"
+            >
+              https://github.com/0xparc/cabal
+            </a>
           </SimpleRow>
           <Row title="Contact Info">
             <div>
@@ -98,7 +112,7 @@ const Homepage = () => {
                   target="_blank"
                   className="hover:text-gray-400"
                 >
-                  twitter: @cabalxyz
+                  Twitter: @cabalxyz
                 </a>
               </span>
             </div>
